64edac793e9bf16f09d248c519b6fdb4c2007458
Letícia <leticia_machadolopes@hotmail.com>
2022-09-02 14:04:40 +0000

    02.09.2022 -> Termino do primeiro CRUD

--- controllers/aluno.js
+++ controllers/aluno.js
@@ -62,12 +62,36 @@ controller.update = async (req, res) => {
     try{
         const response = await Aluno.update(
             req.body,
-            { where: { id: req.body.id }}
+            { where: { id: req.params.id }}
         )
 
         console.log("=====>", {response});
 
         if(response[0] > 0) { // Encontrou e atualizou
+            // HTTP 204: No content
+            res.status(204).end()
+        }
+        else { // Não encontrou (e não atualizou)
+            res.status(404).end()
+        }
+    }
+    catch(error) {
+        console.error(error)
+        // HTTP 500: Internal Server Error
+        res.status(500).send(error)
+    }
+}
+
+controller.delete = async (req, res) => {
+    try{
+        const response = await Aluno.destroy(
+            req.body,
+            { where: { id: req.params.id }}
+        )
+
+        // console.log("=====>", {response});
+
+        if(response) { // Encontrou e atualizou
             // HTTP 204: No content
             res.status(204).end()
         }
